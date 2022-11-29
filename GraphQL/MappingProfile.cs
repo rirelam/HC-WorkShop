9413edd7a63bbd6fa0f4e62eecda0cd1a58ce408
Ricardo Lamar Meneses <r.lamar@habanos.cu>
2022-11-29 17:00:20 -0500

    adicionando automapper para mapeo de datos

---
+++ GraphQL/MappingProfile.cs
@@ -0,0 +1,13 @@
+
+using AutoMapper;
+
+namespace GraphQL
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+        }
+
+    }
+}
--- GraphQL/Program.cs
+++ GraphQL/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
 
 // Add services to the container.
+builder.Services.AddAutoMapper(typeof(Program));
+
 builder.Services.ConfigureLoggerService();
 
 builder.Services.ConfigureDbContextService(builder.Configuration);
